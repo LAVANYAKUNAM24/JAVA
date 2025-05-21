685fce9081b4b69659a2650b229e69b30bdef270
Lavanya <22x01a05a6@nrcmec.org>
2025-05-21 15:44:44 +0530

    hii

---
+++ College/src/Day_9/Demo1.java
@@ -0,0 +1,22 @@
+package Day_9;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Demo1 {
+	
+
+	public static void main(String[] args) {
+		List<Integer> l1=new ArrayList<Integer>();
+		l1.add(12);
+		l1.add(34);
+		l1.add(77);
+		l1.add(88);
+		List<Integer> l2=new ArrayList<Integer>(l1);
+		l2.add(82);
+		l2.add(37);
+		l2.add(77);
+		System.out.println(l2);
+
+	}
+
+}
