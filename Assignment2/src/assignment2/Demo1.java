7b5732655b29818962ce853e01023fe77424458b
Lavanya <22x01a05a6@nrcmec.org>
2025-05-21 15:43:49 +0530

    hi

---
+++ Assignment2/src/assignment2/Demo1.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {44,77,88,99};
+		int a= Integer.parseInt(args[0]);
+		try {
+			System.out.println(arr[a]);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
